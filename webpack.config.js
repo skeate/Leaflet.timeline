f494e0819632dfba1706c66e69901334cbcaa9f4
Jonathan Skeate <skeate@gmail.com>
2017-02-24 23:00:08 -0500

    Update build

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,11 @@ const path = require('path');
 module.exports = {
   entry: {
     'leaflet.timeline': './src/index.js',
-    'leaflet.timeline.min': './src/index.js',
   },
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
 
   module: {
@@ -34,13 +33,6 @@ module.exports = {
       },
     ]
   },
-
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true,
-    }),
-  ],
 
   devtool: 'eval-source-map',
 
